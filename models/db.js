947067db5fdfb1646a04c7776a06a7703c069808
mr-chidex <mbahchiboy2@gmail.com>
2022-05-17 09:06:44 +0100

    ssl to db

--- models/db.js
+++ models/db.js
@@ -4,6 +4,13 @@ const connectionString = process.env.DATABASE_URL || process.env.DEV_DB_URL;
 
 const pool = new Pool({
   connectionString,
+  dialect: "postgresql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = pool;
